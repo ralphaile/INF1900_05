a3ff57e502cf23ead770d61c0c389837910413eb
ireinahedad <113041133+ireinahedad@users.noreply.github.com>
2023-02-17 12:42:39 -0500

    :)

--- tp6/pb1/pb1.cpp
+++ tp6/pb1/pb1.cpp
@@ -1,10 +1,12 @@
+
 #define F_CPU 8000000UL
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 const int DELAI=30;
-volatile uint8_t compteur=0;
-volatile uint8_t etat=0;
+volatile uint8_t counter = 0;
+volatile uint8_t compteurEtat = 0;
+volatile uint8_t gMinuterieExpire = 0;
 void eteindre() 
 {
     PORTB &= ~(PORTB);
@@ -32,16 +34,18 @@ TCCR1C = 0;
 TIMSK1 = (1<<OCIE1A) ;
 }
 
+
 ISR(TIMER1_COMPA_vect) {
-    compteur+=10;
+    gMinuterieExpiree = 1;
+}
+
+ISR(INT0_vect) {
+    gBoutonPoussoir = 1;
+    // anti-rebond
+    _delay_ms(DELAI);
 }
 
 
-ISR (INT0_vect ) {
-   // anti-rebond
-   _delay_ms(DELAI);
-   etat++;
-}
 void initialisation()
 {
     cli();
@@ -51,20 +55,55 @@ void initialisation()
     EICRA |= (1<<ISC00) | (1<<ISC01);
     sei();
 }
-
+enum class Etat { INIT, GREEN, PAUSE, RED, END };
 int main()
 { 
     initialisation();
-    allumerRouge();
-    if (etat==1){
-     for(int i=0;i<12;i+=1)
-        partirMinuterie(7813);
-    }
-    if(compteur==120)
-        eteindre();
-    for(;;){
+    switch (etat) {
+    case Etat::INIT: { //ETAT INIT commence si bouton acctionner, peut diviser dernier etat
+        while (gBoutonPoussoir == 1) {
+            for (int counter = 0, counter < 120, counter++) {
+                partirMinuterie(7813);
+                if (gMinuterieExpire == 1);
+                counter += 10
+            }
+            compteurEtat++
+        }
+        compteurÉtat++    
+        break;
+    case Etat::GREEN:
+         partirMinuterie(7813/2)
+            while(gMinuterieExpire==0)
+            {
 
+            //lumiere clignote vert
+            }
+             compteur++;
+             break;
+    case Etat::PAUSE:
+        partirMinuterie(7813*2);
+            if( gMinuterieExpire==1)
+                compteur++;
+    case Etat::RED:
+        partirMinuterie( counter/(2*2))
+            while(gMinuterieExpire==0)
+            {
+                //lumiere clignote rouge
+            }
+            compteur++
+                
+            break;
+    case END:
+        partirMinuterie(7813)
+            while (gMinuterieExpire == 0) {
+                allumerVert();
+            }
+        if (gboutonpoussoir == 1)
+            compteurEtat = 0
+        else
+            eteindre();
+                break;
     }
-return 0;
 
 }
+

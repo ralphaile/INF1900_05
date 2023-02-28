8921ea9b370eed05c544a79d0989f24857521574
ralphaile <ralph-aile@hotmail.com>
2023-02-27 21:54:19 -0500

    tp6, pb2 removed memoire

--- tp6/pb2/pb2.cpp
+++ tp6/pb2/pb2.cpp
@@ -56,40 +56,14 @@ void initialisation(){
     DDRA &= ~ (1<<DDA0);
     DDRB |= (1<<DDB0) | (1<<DDB1);
 }
-void initialisationUART ( void ) {
-// 2400 bauds. Nous vous donnons la valeur des deux
-// premiers registres pour vous éviter des complications.
-UBRR0H = 0;
-UBRR0L = 0xCF;
-// permettre la réception et la transmission par le UART0
-UCSR0A = 0 ;
-UCSR0B = (1<< RXEN0)| (1<< TXEN0) ;
-// Format des trames: 8 bits, 1 stop bits, sans parité
-UCSR0C = (1<< UCSZ00)|(1<<UCSZ01) ;
-}
-void transmissionUART ( uint8_t donnee ) {
-
-/* Wait for empty transmit buffer */
-while ( !( UCSR0A & (1<<UDRE0)))
-        ;
-/* Copy 9th bit to TXB8 */
-UCSR0B &= ~(1<<TXB80);
-if ( donnee & 0x0100 )
-    UCSR0B |= (1<<TXB80);
-/* Put data into buffer, sends the data */
-UDR0 = donnee;
-UDR0 = '\n';
-}
 
 int main() {
     initialisation();
-    initialisationUART();
     can CAN;
     uint8_t faibleLumiere=200;
     uint8_t forteLumiere=240;
     while(true){
         uint8_t puissance = CAN.lecture(PA0)>>2;
-        transmissionUART(puissance);
         if (puissance < faibleLumiere)
             allumerVert();
         if (faibleLumiere < puissance && puissance < forteLumiere)
